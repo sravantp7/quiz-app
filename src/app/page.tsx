3c96cf73b408cd8049c645eaa2d9f59d2ce0b1c9
Sravan T P <sravantp03@gmail.com>
2023-05-29 17:59:04 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main>
+      <div className='container'>
+        <h1>Quiz App</h1>
+      </div>
+    </main>
+  )
+}
